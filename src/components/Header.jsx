f0247725cbe6e47e72e66793403d23d357191ad9
unnq <63836235+unnq@users.noreply.github.com>
2025-11-17 16:49:14 -0800

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 export default function Header(){
 return (
 <header className="container" style={{display:'flex',justifyContent:'space-between',alignItems:'center',padding:'12px 0'}}>
-<Link to="/" style={{fontWeight:700}}>Jobcito</Link>
+<Link to="/" style={{fontWeight:700}}>smallpay</Link>
 <nav className="row">
 <Link to="/login">Log in</Link>
 <Link className="btn" to="/signup">Create business page</Link>
